1e59a915aab90371aa521c9dfc9c0c6b7b4ad3d7
ju4n1t0x <juanignaciosasia@gmail.com>
2026-09-03 08:42:21 -0300

    fix in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://<tu-usuario>.github.io',
-  base: '/<nombre-repositorio>/',
+  site: 'https://ju4n1t0x.github.io',
+  base: '/lawho/',
   vite: {
     plugins: [tailwindcss()],
   },
