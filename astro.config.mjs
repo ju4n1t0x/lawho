46c07534b9fb8e5fb647d95f40306f507d472c2f
ju4n1t0x <juanignaciosasia@gmail.com>
2026-09-07 14:07:42 -0300

    fix in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ju4n1t0x.github.io',
-  base: '/lawho/',
-  adapter: node({ mode: 'standalone' }),
+  //site: 'https://ju4n1t0x.github.io',
+  //base: '/lawho/',
+  //adapter: node({ mode: 'standalone' }),
   vite: {
     plugins: [tailwindcss()],
   },
